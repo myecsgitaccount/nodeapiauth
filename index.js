e729a6d353cff22deeb6da9de48b2c1bf8684f58
kachilala <mac@macs-MacBook-Pro.local>
2019-11-29 11:41:48 +0100

    add user model and ability to save in mongo atlas

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const app = express(); 
+const mongoose = require('mongoose'); 
+const dotenv = require('dotenv'); 
+
 
 //Import Routes
 const authRoute = require('./routes/auth'); 
 
+dotenv.config(); 
+
+
+// connect to database mongo atlas
+mongoose.connect( process.env.DB_CONNECT, { useNewUrlParser: true },
+    () => console.log('connected to db!')
+); 
+
+// Middleware
+app.use(express.json()); 
+
+
+
 //Routes Middlewares
-app.use('api/user', authRoute);
+app.use('/api/user', authRoute);
 
 app.listen(3000, () => console.log('Server up and running'));
 
